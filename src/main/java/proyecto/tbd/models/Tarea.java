9372c5a327a823c360b1fa6836e29bdd8b54ec61
LatimerV <latimer.vidal@usach.cl>
2019-09-02 14:23:50 -0400

    Modificacion modelo tarea
    Se modifica el modelo tarea, con sus atributos, y se agregan los servicios de los diferentes modelos.

--- src/main/java/proyecto/tbd/models/Tarea.java
+++ src/main/java/proyecto/tbd/models/Tarea.java
@@ -1,4 +1,51 @@
 package proyecto.tbd.models;
 
+import java.util.List;
+
 public class Tarea {
+    private String nombre;
+    private String descripcion;
+    private int cantidad_voluntarios;
+    private List<Voluntario> voluntarios;
+    private List<Caracteristica> caracteristicas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantidad_voluntarios() {
+        return cantidad_voluntarios;
+    }
+
+    public void setCantidad_voluntarios(int cantidad_voluntarios) {
+        this.cantidad_voluntarios = cantidad_voluntarios;
+    }
+
+    public List<Voluntario> getVoluntarios() {
+        return voluntarios;
+    }
+
+    public void setVoluntarios(List<Voluntario> voluntarios) {
+        this.voluntarios = voluntarios;
+    }
+
+    public List<Caracteristica> getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(List<Caracteristica> caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
 }
