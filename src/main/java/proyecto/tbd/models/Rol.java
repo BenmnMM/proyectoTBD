f95f4f70530f3a3660176dd68a91337ef90538d4
Hugo <hugo.arenas@usach.cl>
2019-09-02 18:29:57 -0400

    Rol modificado

--- src/main/java/proyecto/tbd/models/Rol.java
+++ src/main/java/proyecto/tbd/models/Rol.java
@@ -1,4 +1,27 @@
 package proyecto.tbd.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import java.util.Date;
+import java.util.List;
+
 public class Rol {
+	@Id
+    private ObjectId _id;
+    private String nombreR;
+
+    public String get_id(){
+        return _id.toHexString();
+    }
+    public void set_id(ObjectId _id){
+        this._id = _id;
+    }
+    public String getNombreR(){
+        return nombreR;
+    }
+    public void setNombreR(String nombreR){
+        this.nombreR = nombreR;
+    }
 }
