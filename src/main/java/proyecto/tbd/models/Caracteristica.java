b56d7609654ebc0a8ebe9711cf10c3604bfeb5a7
Hugo <hugo.arenas@usach.cl>
2019-09-02 18:21:19 -0400

    modificando características, voluntarios y tareas v2

--- src/main/java/proyecto/tbd/models/Caracteristica.java
+++ src/main/java/proyecto/tbd/models/Caracteristica.java
@@ -15,34 +15,52 @@ public class Caracteristica {
 	private List<Tarea> tareasC;
 	private List<Voluntario> voluntariosC;
 
-    public String get_id()
-    {
+    public String get_id(){
         return _id.toHexString();
     }
-
-    public void set_id(ObjectId _id)
-    {
+    public void set_id(ObjectId _id){
         this._id = _id;
     }
-
-    public String getNombreC()
-    {
-        return ombreC;
+    public String getNombreC(){
+        return nombreC;
     }
-
-    public void setNombreC(String nombreC)
-    {
+    public void setNombreC(String nombreC){
         this.nombreC = nombreC;
     }
-
-    public String getDescripcionC()
-    {
+    public String getDescripcionC(){
         return descripcionC;
     }
-
-    public void setDescripcionC(String descripcionC)
-    {
+    public void setDescripcionC(String descripcionC){
         this.descripcionC = descripcionC;
     }
-
+	public List<Tarea> getTareasC(){
+		return tareasC;
+	}
+	public Tarea getTareaC(int posicion){
+		return tareasC.get(posicion);
+	}
+	public void setTareasC(List<Tarea> tareasC){
+		this.tareasC = tareasC;
+	}
+	public void setTareaC(Tarea tareaC, int posicion){
+		this.tareasC.get(posicion) = tareaC;
+	}
+	public void deleteTareaC(int posicion){
+		this.tareasC.remove(posicion);
+	}
+	public List<Voluntario> getVoluntariosC(){
+		return voluntariosC;
+	}
+	public Voluntario getVoluntarioC(int posicion){
+		return voluntariosC.get(posicion);
+	}
+	public void setVoluntariosC(List<Voluntario> volntariosC){
+		this.voluntariosC = voluntariosC;
+	}
+	public void setVoluntarioC(Voluntario voluntarioC, int posicion){
+		this.voluntariosC.get(posicion) = voluntarioC;
+	}
+	public void deleteVoluntarioC(int posicion){
+		this.voluntariosC.remove(posicion);
+	}
 }
