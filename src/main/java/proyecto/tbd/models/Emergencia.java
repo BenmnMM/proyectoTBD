8e91251a414891c71881bd060271f5787443af15
LatimerV <latimer.vidal@usach.cl>
2019-09-02 17:50:45 -0400

    Modificando emergencia y usuario .p2

--- src/main/java/proyecto/tbd/models/Emergencia.java
+++ src/main/java/proyecto/tbd/models/Emergencia.java
@@ -8,5 +8,27 @@ public class Emergencia {
     private List<Tarea> tareas;
 
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Tarea> getTareas() {
+        return tareas;
+    }
+
+    public void setTareas(List<Tarea> tareas) {
+        this.tareas = tareas;
+    }
 }
--- src/main/java/proyecto/tbd/models/Usuario.java
+++ src/main/java/proyecto/tbd/models/Usuario.java
@@ -9,6 +9,43 @@ public class Usuario {
     private String edad;
     private List<Caracteristica> caracteristicas;
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public List<Caracteristica> getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(List<Caracteristica> caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
 }
