56a7edd7de9452b3301a4f2c7ef20f9d201513eb
LatimerV <latimer.vidal@usach.cl>
2019-09-02 17:48:12 -0400

    Modificiando emergencia y usuario

--- src/main/java/proyecto/tbd/models/Emergencia.java
+++ src/main/java/proyecto/tbd/models/Emergencia.java
@@ -1,7 +1,12 @@
 package proyecto.tbd.models;
 
+import java.util.List;
+
 public class Emergencia {
     private String nombre;
     private String descripcion;
+    private List<Tarea> tareas;
+
+
 
 }
--- src/main/java/proyecto/tbd/models/Usuario.java
+++ src/main/java/proyecto/tbd/models/Usuario.java
@@ -1,4 +1,14 @@
 package proyecto.tbd.models;
 
+import java.util.List;
+
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String edad;
+    private List<Caracteristica> caracteristicas;
+
+
+
 }
